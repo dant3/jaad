1e3bf004b6d7d1dcd682235fa253b64dea468880
in-somnia <in-somnia@e43bebe8-f156-4987-822c-facaa98531cf>
2011-02-24 13:02:35 +0000

    new PS filterbank
    git-svn-id: svn://svn.code.sf.net/p/jaadec/code/jaad@8 e43bebe8-f156-4987-822c-facaa98531cf

--- src/jaad/impl/ps/Filterbank.java
+++ src/jaad/impl/ps/Filterbank.java
@@ -2,304 +2,175 @@ package jaad.impl.ps;
 
 class Filterbank implements PSConstants, FilterbankTables {
 
-	private final float[][] work;
-	private final float[][][] buffer, temp;
-	private final float[][] bufa;
-	private final float[][] buf12a, buf12b; //extra buffer for channelFilter12
-	private final float[] buf8; //extra buffer for channelFilter8
-	private final float[] dctBuf; //shared buffer for DCT3-4 and DCT3-6
+    private static final int FB_LEN = 32;
+    private float[][][] buf;
 
-	Filterbank() {
-		work = new float[TIME_SLOTS_RATE+12][2];
-		buffer = new float[5][TIME_SLOTS_RATE][2];
-		temp = new float[TIME_SLOTS_RATE][12][2];
-		bufa = new float[7][7];
-		buf8 = new float[4];
-		buf12a = new float[6][2];
-		buf12b = new float[6][2];
-		dctBuf = new float[7];
-	}
+    Filterbank() {
+        buf = new float[5][44][2];
+    }
 
-	public void performAnalysis(float[][][] in, float[][][] out, boolean use34) {
-		final int[] resolution = (use34) ? RESOLUTION34 : RESOLUTION20;
-		int offset = 0;
+    void performAnalysis(float[][][] in, float[][][] out, boolean use34) {
+        int i, j;
+        for (i = 0; i < 5; i++) {
+            for (j = 0; j < 38; j++) {
+                in[i][j + 6][0] = buf[j][i][0];
+                in[i][j + 6][1] = buf[j][i][1];
+            }
+        }
+        if (use34) {
+            performChannelFilter4(in[0], out, 0, F34_0_12, 12, FB_LEN);
+            performChannelFilter4(in[1], out, 12, F34_1_8, 8, FB_LEN);
+            performChannelFilter4(in[2], out, 20, F34_2_4, 4, FB_LEN);
+            performChannelFilter4(in[3], out, 24, F34_2_4, 4, FB_LEN);
+            performChannelFilter4(in[4], out, 28, F34_2_4, 4, FB_LEN);
+            for (i = 0; i < 59; i++) {
+                for (j = 0; j < FB_LEN; j++) {
+                    out[i + 32][j][0] = buf[0][j][i + 5];
+                    out[i + 32][j][1] = buf[1][j][i + 5];
+                }
+            }
+        } else {
+            performChannelFilter6(in[0], out, 0, F20_0_8, FB_LEN);
+            performChannelFilter2(in[1], out, 6, G1_Q2, FB_LEN, true);
+            performChannelFilter2(in[2], out, 8, G1_Q2, FB_LEN, false);
+            for (i = 0; i < 61; i++) {
+                for (j = 0; j < FB_LEN; j++) {
+                    out[i + 10][j][0] = buf[0][j][i + 3];
+                    out[i + 10][j][1] = buf[1][j][i + 3];
+                }
+            }
+        }
+        //update in_buf
+        for (i = 0; i < 5; i++) {
+            System.arraycopy(in[i], 0, in[i + 32], 0, 6);
+            //memcpy(in[i], in[i]+32, 6 * sizeof(in[i][0]));
+        }
+    }
 
-		int i, j;
-		for(int band = 0; band<resolution.length; band++) {
-			//build working buffer (complex copy)
-			for(i = 0; i<12; i++) {
-				work[i][0] = buffer[band][i][0];
-				work[i][1] = buffer[band][i][1];
-			}
+    void performSynthesis(float[][][] in, float[][][] out, boolean use34) {
+        int i, n;
+        if (use34) {
+            for (n = 0; n < FB_LEN; n++) {
+                for (i = 0; i < 12; i++) {
+                    out[n][0][0] += in[i][n][0];
+                    out[n][0][1] += in[i][n][1];
+                }
+                for (i = 0; i < 8; i++) {
+                    out[n][1][0] += in[12 + i][n][0];
+                    out[n][1][1] += in[12 + i][n][1];
+                }
+                for (i = 0; i < 4; i++) {
+                    out[n][2][0] += in[20 + i][n][0];
+                    out[n][2][1] += in[20 + i][n][1];
+                    out[n][3][0] += in[24 + i][n][0];
+                    out[n][3][1] += in[24 + i][n][1];
+                    out[n][4][0] += in[28 + i][n][0];
+                    out[n][4][1] += in[28 + i][n][1];
+                }
+            }
+            for (i = 0; i < 59; i++) {
+                for (n = 0; n < FB_LEN; n++) {
+                    out[n][i + 5][0] = in[i + 32][n][0];
+                    out[n][i + 5][1] = in[i + 32][n][1];
+                }
+            }
+        } else {
+            for (n = 0; n < FB_LEN; n++) {
+                out[n][0][0] = in[0][n][0] + in[1][n][0] + in[2][n][0]
+                        + in[3][n][0] + in[4][n][0] + in[5][n][0];
+                out[n][0][1] = in[0][n][1] + in[1][n][1] + in[2][n][1]
+                        + in[3][n][1] + in[4][n][1] + in[5][n][1];
+                out[n][1][0] = in[6][n][0] + in[7][n][0];
+                out[n][1][1] = in[6][n][1] + in[7][n][1];
+                out[n][2][0] = in[8][n][0] + in[9][n][0];
+                out[n][2][1] = in[8][n][1] + in[9][n][1];
+            }
+            for (i = 0; i < 61; i++) {
+                for (n = 0; n < FB_LEN; n++) {
+                    out[n][i + 3][0] = in[i + 10][n][0];
+                    out[n][i + 3][1] = in[i + 10][n][1];
+                }
+            }
+        }
+    }
 
-			//add new samples
-			for(i = 0; i<TIME_SLOTS_RATE; i++) {
-				work[12+i][0] = in[i+ANALYSIS_DELAY][band][0];
-				work[12+i][1] = in[i+ANALYSIS_DELAY][band][1];
-			}
+    /** Split one subband into 2 subsubbands with a symmetric real filter.
+     * The filter must have its non-center even coefficients equal to zero. */
+    private void performChannelFilter2(float[][] in, float[][][] out, int outOff, float[] filter, int len, boolean reverse) {
+        int inOff = 0;
+        int j;
+        for (int i = 0; i < len; i++, inOff++) {
+            float re_in = filter[6] * in[inOff + 6][0];
+            float re_op = 0.0f;
+            float im_in = filter[6] * in[inOff + 6][1];
+            float im_op = 0.0f;
+            for (j = 0; j < 6; j += 2) {
+                re_op += filter[j + 1] * (in[inOff + j + 1][0] + in[inOff + 12 - j - 1][0]);
+                im_op += filter[j + 1] * (in[inOff + j + 1][1] + in[inOff + 12 - j - 1][1]);
+            }
+            int x = reverse ? 1 : 0;
+            out[outOff + x][i][0] = re_in + re_op;
+            out[outOff + x][i][1] = im_in + im_op;
+            x = reverse ? 0 : 1;
+            out[outOff + x][i][0] = re_in - re_op;
+            out[outOff + x][i][1] = im_in - im_op;
+        }
+    }
 
-			//store samples (complex copy)
-			for(i = 0; i<12; i++) {
-				buffer[band][i][0] = work[TIME_SLOTS_RATE+i][0];
-				buffer[band][i][1] = work[TIME_SLOTS_RATE+i][1];
-			}
+    private void performChannelFilter4(float[][] in, float[][][] out, int outOff, float[][][] filter, int N, int len) {
+        int inOff = 0;
+        int i, j, ssb;
 
-			switch(resolution[band]) {
-				case 2:
-					performChannelFilter2(P2_13_20);
-					break;
-				case 4:
-					performChannelFilter4(P4_13_34);
-					break;
-				case 8:
-					performChannelFilter8((use34) ? P8_13_34 : P8_13_20);
-					break;
-				case 12:
-					performChannelFilter12(P12_13_34);
-					break;
-			}
+        for (i = 0; i < len; i++, inOff++) {
+            for (ssb = 0; ssb < N; ssb++) {
+                float sum_re = filter[ssb][6][0] * in[inOff + 6][0], sum_im = filter[ssb][6][0] * in[inOff + 6][1];
+                for (j = 0; j < 6; j++) {
+                    float in0_re = in[inOff + j][0];
+                    float in0_im = in[inOff + j][1];
+                    float in1_re = in[inOff + 12 - j][0];
+                    float in1_im = in[inOff + 12 - j][1];
+                    sum_re += filter[ssb][j][0] * (in0_re + in1_re) - filter[ssb][j][1] * (in0_im - in1_im);
+                    sum_im += filter[ssb][j][0] * (in0_im + in1_im) + filter[ssb][j][1] * (in0_re - in1_re);
+                }
+                out[outOff + ssb][i][0] = sum_re;
+                out[outOff + ssb][i][1] = sum_im;
+            }
+        }
+    }
 
-			for(i = 0; i<TIME_SLOTS_RATE; i++) {
-				for(j = 0; j<resolution[band]; j++) {
-					out[i][offset+j][0] = temp[i][j][0];
-					out[i][offset+j][1] = temp[i][j][1];
-				}
-			}
-			offset += resolution[band];
-		}
+    /** Split one subband into 6 subsubbands with a complex filter */
+    private void performChannelFilter6(float[][] in, float[][][] out, int outOff, float[][][] filter, int len) {
+        int i, j, ssb;
+        int N = 8;
+        float[][] temp = new float[8][2];
 
-		//group hybrid channels
-		if(!use34) {
-			for(i = 0; i<TIME_SLOTS_RATE; i++) {
-				out[i][3][0] += out[i][4][0];
-				out[i][3][1] += out[i][4][1];
-				out[i][4][0] = 0;
-				out[i][4][1] = 0;
-
-				out[i][2][0] += out[i][5][0];
-				out[i][2][1] += out[i][5][1];
-				out[i][5][0] = 0;
-				out[i][5][1] = 0;
-			}
-		}
-	}
-
-	public void performSynthesis(float[][][] in, float[][][] out, boolean use34) {
-		final int[] resolution = (use34) ? RESOLUTION34 : RESOLUTION20;
-		int offset = 0;
-
-		int i, j;
-		for(int band = 0; band<resolution.length; band++) {
-			for(i = 0; i<TIME_SLOTS_RATE; i++) {
-				out[i][band][0] = 0;
-				out[i][band][1] = 0;
-
-				for(j = 0; j<resolution[band]; j++) {
-					out[i][band][0] += in[i][offset+j][0];
-					out[i][band][1] += in[i][offset+j][1];
-				}
-			}
-			offset += resolution[band];
-		}
-	}
-
-	//real filter, size 2
-	private void performChannelFilter2(float[] filter) {
-		for(int i = 0; i<TIME_SLOTS_RATE; i++) {
-			bufa[0][0] = filter[0]*(work[0+i][0]+work[12+i][0]);
-			bufa[0][1] = filter[1]*(work[1+i][0]+work[11+i][0]);
-			bufa[0][2] = filter[2]*(work[2+i][0]+work[10+i][0]);
-			bufa[0][3] = filter[3]*(work[3+i][0]+work[9+i][0]);
-			bufa[0][4] = filter[4]*(work[4+i][0]+work[8+i][0]);
-			bufa[0][5] = filter[5]*(work[5+i][0]+work[7+i][0]);
-			bufa[0][6] = filter[6]*work[6+i][0];
-			bufa[1][0] = filter[0]*(work[0+i][1]+work[12+i][1]);
-			bufa[1][1] = filter[1]*(work[1+i][1]+work[11+i][1]);
-			bufa[1][2] = filter[2]*(work[2+i][1]+work[10+i][1]);
-			bufa[1][3] = filter[3]*(work[3+i][1]+work[9+i][1]);
-			bufa[1][4] = filter[4]*(work[4+i][1]+work[8+i][1]);
-			bufa[1][5] = filter[5]*(work[5+i][1]+work[7+i][1]);
-			bufa[1][6] = filter[6]*work[6+i][1];
-
-			temp[i][0][0] = bufa[0][0]+bufa[0][1]+bufa[0][2]+bufa[0][3]+bufa[0][4]+bufa[0][5]+bufa[0][6];
-			temp[i][0][1] = bufa[1][0]+bufa[1][1]+bufa[1][2]+bufa[1][3]+bufa[1][4]+bufa[1][5]+bufa[1][6];
-
-			temp[i][1][0] = bufa[0][0]-bufa[0][1]+bufa[0][2]-bufa[0][3]+bufa[0][4]-bufa[0][5]+bufa[0][6];
-			temp[i][1][1] = bufa[1][0]-bufa[1][1]+bufa[1][2]-bufa[1][3]+bufa[1][4]-bufa[1][5]+bufa[1][6];
-		}
-	}
-
-	//complex filter, size 4
-	private void performChannelFilter4(float[] filter) {
-		for(int i = 0; i<TIME_SLOTS_RATE; i++) {
-			bufa[0][0] = -(filter[2]*(work[i+2][0]+work[i+10][0]))
-					+(filter[6]*work[i+6][0]);
-			bufa[0][1] = -0.70710678118655f
-					*((filter[1]*(work[i+1][0]+work[i+11][0]))
-					+(filter[3]*(work[i+3][0]+work[i+9][0]))
-					-(filter[5]*(work[i+5][0]+work[i+7][0])));
-
-			bufa[1][0] = (filter[0]*(work[i+0][1]-work[i+12][1]))
-					-(filter[4]*(work[i+4][1]-work[i+8][1]));
-			bufa[1][1] = 0.70710678118655f
-					*((filter[1]*(work[i+1][1]-work[i+11][1]))
-					-(filter[3]*(work[i+3][1]-work[i+9][1]))
-					-(filter[5]*(work[i+5][1]-work[i+7][1])));
-
-			bufa[2][0] = (filter[0]*(work[i+0][0]-work[i+12][0]))
-					-(filter[4]*(work[i+4][0]-work[i+8][0]));
-			bufa[2][1] = 0.70710678118655f
-					*((filter[1]*(work[i+1][0]-work[i+11][0]))
-					-(filter[3]*(work[i+3][0]-work[i+9][0]))
-					-(filter[5]*(work[i+5][0]-work[i+7][0])));
-
-			bufa[3][0] = -(filter[2]*(work[i+2][1]+work[i+10][1]))
-					+(filter[6]*work[i+6][1]);
-			bufa[3][1] = -0.70710678118655f
-					*((filter[1]*(work[i+1][1]+work[i+11][1]))
-					+(filter[3]*(work[i+3][1]+work[i+9][1]))
-					-(filter[5]*(work[i+5][1]+work[i+7][1])));
-
-			temp[i][0][0] = bufa[0][0]+bufa[0][1]+bufa[1][0]+bufa[1][1];
-			temp[i][0][1] = -bufa[2][0]-bufa[2][1]+bufa[3][0]+bufa[3][1];
-
-			temp[i][1][0] = bufa[0][0]-bufa[0][1]-bufa[1][0]+bufa[1][1];
-			temp[i][1][1] = bufa[2][0]-bufa[2][1]+bufa[3][0]-bufa[3][1];
-
-			temp[i][2][0] = bufa[0][0]-bufa[0][1]+bufa[1][0]-bufa[1][1];
-			temp[i][2][1] = -bufa[2][0]+bufa[2][1]+bufa[3][0]-bufa[3][1];
-
-			temp[i][3][0] = bufa[0][0]+bufa[0][1]-bufa[1][0]-bufa[1][1];
-			temp[i][3][1] = bufa[2][0]+bufa[2][1]+bufa[3][0]+bufa[3][1];
-		}
-	}
-
-	//complex filter, size 8
-	private void performChannelFilter8(float[] filter) {
-		int n;
-		for(int i = 0; i<TIME_SLOTS_RATE; i++) {
-			bufa[0][0] = filter[6]*work[6+i][0];
-			bufa[0][1] = (filter[5]*(work[5+i][0]+work[7+i][0]));
-			bufa[0][2] = -(filter[0]*(work[0+i][0]+work[12+i][0]))+(filter[4]*(work[4+i][0]+work[8+i][0]));
-			bufa[0][3] = -(filter[1]*(work[1+i][0]+work[11+i][0]))+(filter[3]*(work[3+i][0]+work[9+i][0]));
-
-			bufa[1][0] = (filter[5]*(work[7+i][1]-work[5+i][1]));
-			bufa[1][1] = (filter[0]*(work[12+i][1]-work[0+i][1]))+(filter[4]*(work[8+i][1]-work[4+i][1]));
-			bufa[1][2] = (filter[1]*(work[11+i][1]-work[1+i][1]))+(filter[3]*(work[9+i][1]-work[3+i][1]));
-			bufa[1][3] = (filter[2]*(work[10+i][1]-work[2+i][1]));
-
-			for(n = 0; n<4; n++) {
-				buf8[n] = bufa[0][n]-bufa[1][3-n];
-			}
-			computeDCT3L4(buf8);
-			temp[i][7][0] = buf8[0];
-			temp[i][5][0] = buf8[2];
-			temp[i][3][0] = buf8[3];
-			temp[i][1][0] = buf8[1];
-
-			for(n = 0; n<4; n++) {
-				buf8[n] = bufa[0][n]+bufa[1][3-n];
-			}
-			computeDCT3L4(buf8);
-			temp[i][6][0] = buf8[1];
-			temp[i][4][0] = buf8[3];
-			temp[i][2][0] = buf8[2];
-			temp[i][0][0] = buf8[0];
-
-			bufa[1][0] = (filter[6]*work[6+i][1]);
-			bufa[1][1] = (filter[5]*(work[5+i][1]+work[7+i][1]));
-			bufa[1][2] = -(filter[0]*(work[0+i][1]+work[12+i][1]))+(filter[4]*(work[4+i][1]+work[8+i][1]));
-			bufa[1][3] = -(filter[1]*(work[1+i][1]+work[11+i][1]))+(filter[3]*(work[3+i][1]+work[9+i][1]));
-
-			bufa[0][0] = (filter[5]*(work[7+i][0]-work[5+i][0]));
-			bufa[0][1] = (filter[0]*(work[12+i][0]-work[0+i][0]))+(filter[4]*(work[8+i][0]-work[4+i][0]));
-			bufa[0][2] = (filter[1]*(work[11+i][0]-work[1+i][0]))+(filter[3]*(work[9+i][0]-work[3+i][0]));
-			bufa[0][3] = (filter[2]*(work[10+i][0]-work[2+i][0]));
-
-			for(n = 0; n<4; n++) {
-				buf8[n] = bufa[1][n]+bufa[0][3-n];
-			}
-			computeDCT3L4(buf8);
-			temp[i][7][1] = buf8[0];
-			temp[i][5][1] = buf8[2];
-			temp[i][3][1] = buf8[3];
-			temp[i][1][1] = buf8[1];
-
-			for(n = 0; n<4; n++) {
-				buf8[n] = bufa[1][n]-bufa[0][3-n];
-			}
-			computeDCT3L4(buf8);
-			temp[i][6][1] = buf8[1];
-			temp[i][4][1] = buf8[3];
-			temp[i][2][1] = buf8[2];
-			temp[i][0][1] = buf8[0];
-		}
-	}
-
-	//complex filter, size 12
-	private void performChannelFilter12(float[] filter) {
-		int n;
-		for(int i = 0; i<TIME_SLOTS_RATE; i++) {
-			for(n = 0; n<6; n++) {
-				if(n==0) {
-					bufa[0][0] = work[6+i][0]*filter[6];
-					bufa[2][0] = work[6+i][1]*filter[6];
-				}
-				else {
-					bufa[0][6-n] = (work[n+i][0]+work[12-n+i][0])*filter[n];
-					bufa[2][6-n] = (work[n+i][1]+work[12-n+i][1])*filter[n];
-				}
-				bufa[3][n] = (work[n+i][0]-work[12-n+i][0])*filter[n];
-				bufa[1][n] = (work[n+i][1]-work[12-n+i][1])*filter[n];
-			}
-
-			computeDCT3L6(bufa[0], buf12a[0]);
-			computeDCT3L6(bufa[2], buf12b[0]);
-
-			computeDCT3L6(bufa[1], buf12a[1]);
-			computeDCT3L6(bufa[3], buf12b[1]);
-
-			for(n = 0; n<6; n += 2) {
-				temp[i][n][0] = buf12a[0][n]-buf12a[1][n];
-				temp[i][n][1] = buf12b[0][n]+buf12b[1][n];
-				temp[i][n+1][0] = buf12a[0][n+1]+buf12a[1][n+1];
-				temp[i][n+1][1] = buf12b[0][n+1]-buf12b[1][n+1];
-
-				temp[i][10-n][0] = buf12a[0][n+1]-buf12a[1][n+1];
-				temp[i][10-n][1] = buf12b[0][n+1]+buf12b[1][n+1];
-				temp[i][11-n][0] = buf12a[0][n]+buf12a[1][n];
-				temp[i][11-n][1] = buf12b[0][n]-buf12b[1][n];
-			}
-		}
-	}
-
-	private void computeDCT3L4(float[] f) {
-		dctBuf[0] = f[3]-(DCT3_4_TABLE[0]*f[1]);
-		dctBuf[1] = (DCT3_4_TABLE[1]*dctBuf[0])+f[1];
-		dctBuf[2] = dctBuf[0]-(DCT3_4_TABLE[2]*dctBuf[1]);
-		dctBuf[0] = DCT3_4_TABLE[3]*f[2];
-		dctBuf[3] = f[0]+dctBuf[0];
-		dctBuf[4] = f[0]-dctBuf[0];
-		f[0] = dctBuf[3]+dctBuf[1];
-		f[3] = dctBuf[3]-dctBuf[1];
-		f[2] = dctBuf[4]+dctBuf[2];
-		f[1] = dctBuf[4]-dctBuf[2];
-	}
-
-	private void computeDCT3L6(float[] in, float[] out) {
-		dctBuf[0] = in[3]*DCT3_6_TABLE[0];
-		dctBuf[1] = in[0]+dctBuf[0];
-		dctBuf[2] = in[0]-dctBuf[0];
-		dctBuf[3] = (in[1]-in[5])*DCT3_6_TABLE[1];
-		dctBuf[4] = (in[2]*DCT3_6_TABLE[2])+(in[4]*DCT3_6_TABLE[3]);
-		dctBuf[5] = dctBuf[4]-in[4];
-		dctBuf[6] = (in[1]*DCT3_6_TABLE[4])+(in[5]*DCT3_6_TABLE[5]);
-		dctBuf[7] = dctBuf[6]-dctBuf[3];
-		out[0] = dctBuf[1]+dctBuf[6]+dctBuf[4];
-		out[1] = dctBuf[2]+dctBuf[3]-in[4];
-		out[2] = dctBuf[7]+dctBuf[2]-dctBuf[5];
-		out[3] = dctBuf[1]-dctBuf[7]-dctBuf[5];
-		out[4] = dctBuf[1]-dctBuf[3]-in[4];
-		out[5] = dctBuf[2]-dctBuf[6]+dctBuf[4];
-	}
+        int inOff = 0;
+        for (i = 0; i < len; i++, inOff++) {
+            for (ssb = 0; ssb < N; ssb++) {
+                float sum_re = filter[ssb][6][0] * in[inOff + 6][0], sum_im = filter[ssb][6][0] * in[inOff + 6][1];
+                for (j = 0; j < 6; j++) {
+                    float in0_re = in[inOff + j][0];
+                    float in0_im = in[inOff + j][1];
+                    float in1_re = in[inOff + 12 - j][0];
+                    float in1_im = in[inOff + 12 - j][1];
+                    sum_re += filter[ssb][j][0] * (in0_re + in1_re) - filter[ssb][j][1] * (in0_im - in1_im);
+                    sum_im += filter[ssb][j][0] * (in0_im + in1_im) + filter[ssb][j][1] * (in0_re - in1_re);
+                }
+                temp[ssb][0] = sum_re;
+                temp[ssb][1] = sum_im;
+            }
+            out[outOff + 0][i][0] = temp[6][0];
+            out[outOff + 0][i][1] = temp[6][1];
+            out[outOff + 1][i][0] = temp[7][0];
+            out[outOff + 1][i][1] = temp[7][1];
+            out[outOff + 2][i][0] = temp[0][0];
+            out[outOff + 2][i][1] = temp[0][1];
+            out[outOff + 3][i][0] = temp[1][0];
+            out[outOff + 3][i][1] = temp[1][1];
+            out[outOff + 4][i][0] = temp[2][0] + temp[5][0];
+            out[outOff + 4][i][1] = temp[2][1] + temp[5][1];
+            out[outOff + 5][i][0] = temp[3][0] + temp[4][0];
+            out[outOff + 5][i][1] = temp[3][1] + temp[4][1];
+        }
+    }
 }
