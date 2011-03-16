3719d77197731aae3d073dd6c23dd8b1b50b8e9b
in-somnia <in-somnia@e43bebe8-f156-4987-822c-facaa98531cf>
2011-03-16 11:11:08 +0000

    new directory structure (3)
    git-svn-id: svn://svn.code.sf.net/p/jaadec/code/jaad@24 e43bebe8-f156-4987-822c-facaa98531cf

---
+++ test/net/sourceforge/jaad/impl/sbr/FilterbankTest.java
@@ -0,0 +1,98 @@
+package jaad.impl.sbr;
+
+import jaad.impl.TestUtils;
+import org.junit.Test;
+
+public class FilterbankTest implements FilterbankTables {
+
+	private static final int[] BIT_REVERSE = {0, 16, 24, 8, 28, 12, 20, 4, 30, 14, 22, 6, 26, 10, 18,
+		2, 31, 15, 23, 7, 27, 11, 19, 3, 29, 13, 21, 5, 25, 9, 17, 1};
+
+	@Test
+	public void testDST() {
+		final float[] in = TestUtils.generateRandomVector(32);
+		final float[] ref = computeRefDST(in);
+
+		//new QMFSynthesis(new Filterbank(), 32).computeDST(in); //for testing: remove private modifier from method
+
+		TestUtils.compare(ref, in);
+	}
+
+	private float[] computeRefDST(float[] in) {
+		final int len = in.length;
+		final float PIN = (float) (Math.PI/(float) len);
+		final float[] out = new float[len];
+
+		for(int k = 0; k<len; k++) {
+			out[k] = 0;
+			for(int n = 0; n<len; n++) {
+				out[k] += in[n]*Math.sin(PIN*(n+0.5)*(k+0.5));
+			}
+		}
+
+		return out;
+	}
+
+	@Test
+	public void testDCT() {
+		final float[] in = TestUtils.generateRandomVector(32);
+		final float[] ref = computeRefDCT(in);
+
+		//new QMFSynthesis(new Filterbank(), 32).computeDCT(in); //for testing: remove private modifier from method
+
+		TestUtils.compare(ref, in);
+	}
+
+	//DCT-IV
+	private float[] computeRefDCT(float[] in) {
+		final int len = in.length;
+		final float PIN = (float) (Math.PI/(float) len);
+		final float[] out = new float[len];
+
+		for(int k = 0; k<len; k++) {
+			out[k] = 0;
+			for(int n = 0; n<len; n++) {
+				out[k] += in[n]*Math.cos(PIN*(n+0.5)*(k+0.5));
+			}
+		}
+
+		return out;
+	}
+
+	@Test
+	public void testFFT() {
+		final float[][] in = TestUtils.generateRandomVectorC(32);
+		final float[][] ref = computeRefDFT(in);
+
+		//Filterbank.computeFFT(in); //for testing: remove private modifier from FFT
+		float[][] out = new float[32][2];
+		for(int i = 0; i<32; i++) {
+			out[BIT_REVERSE[i]] = in[i];
+		}
+
+		TestUtils.compare(ref, out);
+	}
+
+	private float[][] computeRefDFT(float[][] in) {
+		final int len = in.length;
+		final float[][] out = new float[len][2];
+		final float PI2 = (float) Math.PI*2.0f;
+
+		float real, imag, phase, sin, cos;
+		for(int k = 0; k<len; k++) {
+			real = 0;
+			imag = 0;
+			for(int j = 0; j<len; j++) {
+				phase = PI2*((k*j)/(float) len);
+				sin = (float) Math.sin(phase);
+				cos = (float) Math.cos(phase);
+				real += (cos*in[j][0])-(sin*in[j][1]);
+				imag += (cos*in[j][1])+(sin*in[j][0]);
+			}
+			out[k][0] = real;
+			out[k][1] = imag;
+		}
+
+		return out;
+	}
+}
